2d04efad3bac7681d1f1b2178827da39b80b6591
sedkitech <dhiasedkipro@gmail.com>
2023-02-16 16:37:48 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,17 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int i = 0;
-
-	while (ch[i] != '\0')
-	{
-	putchar (ch[i]);
-	i++;
-	}
-	putchar('\n');
-	return (0);
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    fprintf(stderr, "%s", message);
+    return (1);
 }
