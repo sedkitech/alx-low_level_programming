0e7f0db781b8bedf5b33f8e8b7fd377f7ab958eb
sedkitech <dhiasedkipro@gmail.com>
2023-02-21 11:22:52 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,9 @@
-#include <stdio.h>
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
 
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-}
-
-int main(void)
-{
-print_alphabet();
-return (0);
+		_putchar(i);
 }
