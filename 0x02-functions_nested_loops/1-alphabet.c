779628a7485738fd013a28ad200e40de17f3c4c2
sedkitech <dhiasedkipro@gmail.com>
2023-02-21 11:32:57 -0800

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: a function that prints alphabet words
+ *
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet(void)
 {
 	int i;
