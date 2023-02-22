d1bdc3940f7c3d280eb95e861d9c226b06185cbb
sedkitech <dhiasedkipro@gmail.com>
2023-02-22 06:43:40 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,15 @@
 int print_last_digit(int n)
 {
 	int last_int = n % 10;
-	char c = last_int + '0';
+	char c;
 
+	if (last_int < 0)
+	{
+		last_int = last_int * -1;
+	}
+
+
+	c = last_int + '0';
 	_putchar(c);
 	return (last_int);
 }
