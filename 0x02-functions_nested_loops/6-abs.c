d8085dca6706b3ec1ac1ecd45d2283c918973558
sedkitech <dhiasedkipro@gmail.com>
2023-02-22 02:57:42 -0800

    adding abs func

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of an integer
+ * @n: the number to check
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (n * -1);
+}
