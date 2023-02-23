f506b15c1e16c330a055ad31470a4503e68e662d
sedkitech <dhiasedkipro@gmail.com>
2023-02-23 08:42:40 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - draw Entry point
+ * Description: fizz buzz test is an interview question
+ * Return: always 0
+ */
+
+int main(void)
+{
+	 int i;
+         int j;
+
+        for (i = 1; i <= 100; i++)
+        {
+            if (((i % 5) == 0 ) && ((i % 3) == 0))
+            printf ("FizzBuzz ");
+            else if ((i % 3) == 0)
+            printf ("Fizz ");
+            else if ((i % 5) == 0)
+            printf ("Buzz ");
+            else
+            {
+            printf ("%d", i);
+            printf (" ");
+            }
+        }
+return (0);
+}
