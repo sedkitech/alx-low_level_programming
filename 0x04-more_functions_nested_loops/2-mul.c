a7f545b5f915f28ae6f022a7e8f33e4e43775781
sedkitech <dhiasedkipro@gmail.com>
2023-02-23 05:11:34 -0800

    multipli func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to multipli with b
+ * @b: interger to multipli with a
+ * Return: 0 or 1
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
