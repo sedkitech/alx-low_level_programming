fcb7b3089e0b2966c3eaf42cf58e5dc9813c6775
sedkitech <dhiasedkipro@gmail.com>
2023-02-23 07:08:24 -0800

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line -print lines
+ *@n: number of lines
+ * Return: always 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
