2541a4cc77e577a5a7f0e6388695d67fc1a68e40
sedkitech <dhiasedkipro@gmail.com>
2023-02-23 06:21:59 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,21 @@
 
 void more_numbers(void)
 {
-int i;
-int j = 0;
+	int i;
+	int j = 0;
 
-while (j <= 9)
-{
-for (i = '0'; i <= '9'; i++)
-{
-_putchar(i);
-}
-for (i = '0'; i <= '4'; i++)
-{
-_putchar('1');
-_putchar(i);
-}
-_putchar('\n');
-j++;
-}
+	while (j <= 9)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+		for (i = '0'; i <= '4'; i++)
+		{
+			_putchar('1');
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
 }
