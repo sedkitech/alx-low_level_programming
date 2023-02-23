631c3303e41c4e0ecf59faef1559878874857a5f
sedkitech <dhiasedkipro@gmail.com>
2023-02-23 06:10:37 -0800

    more numbers func

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -print numbers
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+int i;
+int j = 0;
+
+while (j <= 10)
+{
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+for (i = '0'; i <= '4'; i++)
+{
+_putchar('1');
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
+}
