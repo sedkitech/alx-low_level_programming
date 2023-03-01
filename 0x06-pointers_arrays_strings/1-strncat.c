39c5a4d0943f38ad52ffda93d5349b27080b1494
sedkitech <92572909+sedkitech@users.noreply.github.com>
2023-03-01 20:40:58 +0100

    Create strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings with n length
+ *@dest: char
+ *@src: char
+ *@n: int
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] <= n; ++i)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
