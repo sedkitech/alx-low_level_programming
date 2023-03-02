9eab9941648f39a32c11773df9c9eeff7106b171
sedkitech <dhiasedkipro@gmail.com>
2023-03-02 15:14:03 -0800

    fixing strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,23 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int len = strlen(dest);
 	int i;
-
-	for (i = 0; i < n; ++i)
+	int len_src = strlen(src);
+	
+	if (n <= len_src)
 	{
+		for (i = 0; i < n; ++i)
+		{
 		dest[len] = src[i];
 		len++;
+		}
+	}
+	else 
+	{
+		for (i = 0; i < len_src; ++i)
+		{
+			dest[len] = src[i];
+			len++;
+		}
 	}
 	dest[len] = '\0';
 	return (dest);
