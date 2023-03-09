2ec155b21eff0f9b3ff0eebfac1a2dc047ba39df
sedkitech <dhiasedkipro@gmail.com>
2023-03-09 08:09:39 -0800

    adding puts-rec func

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (strlen(s) == 0)
+	{
+		printf("\n");
+		return;
+	}
+	printf("%c",s[0]);
+	_puts_recursion(s + 1);
+}
