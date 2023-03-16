fb4250ffdf2176b7f0577d602551d0a129b655e2
sedkitech <dhiasedkipro@gmail.com>
2023-03-15 17:02:19 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	arr = malloc(strlen(s1) + strlen(s2));
+	arr = malloc(strlen(s1) + strlen(s2) + 1);
 	if (!arr)
 		return (NULL);
 
