a6749a7b838d4a4a1a025adf46f19b42fc30bc14
sedkitech <dhiasedkipro@gmail.com>
2023-03-17 17:57:50 -0700

    adding calloc func

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of the element
+ * Return: A pointer or NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (!nmemb || !size)
+		return (NULL);
+
+	p = calloc(nmemb, size);
+
+	if (!p)
+		return (NULL);
+
+	return (p);
+}
