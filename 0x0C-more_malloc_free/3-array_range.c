61f4b9dd6ba34b3c575aba6cdaa1ce9d534bd55f
sedkitech <dhiasedkipro@gmail.com>
2023-03-17 17:59:43 -0700

    adding arra-range func

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: min number
+ * @max: maximum number
+ * Return: A pointer or NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int i, len;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min;
+	array = malloc((len + 1) * sizeof(int));
+
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		array[i] = min++;
+
+	return (array);
+
+}
