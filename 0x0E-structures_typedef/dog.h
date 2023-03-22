4f9d5e25145bcbe5b25c452de3496d2407ccdc5f
sedkitech <dhiasedkipro@gmail.com>
2023-03-21 18:37:35 -0700

    optimizing typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,14 @@
  * Description: elements of a dog struct
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
 
-typedef dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
