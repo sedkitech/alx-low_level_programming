324429fda5da253d25c83bfcfd55ee0786bc50f2
sedkitech <dhiasedkipro@gmail.com>
2023-03-25 03:49:23 -0700

    optimizing sum function and variadic_functions.h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,25 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+
 /**
- * sum_them_all - sum all it's arguments
- * @n: number of arguments
- * @...: arguments
- * Return: sum of all arguments or 0
+ * sum_them_all - returns the sum of args
+ * @n: number of args
+ * @...: args
+ * Return: sum or 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int i = 0;
+	va_list args;
 	int sum = 0;
-
-	if (!n)
+	unsigned int i;
+	
+	if (n == 0)
 		return (0);
 
-	va_start(list, n);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
 
-	for (; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
-
+	va_end(args);
 	return (sum);
-
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /* VARIADIC_FUNCTIONS_H */
+
+#endif /* VARIADIC_FUNCTIONS */
