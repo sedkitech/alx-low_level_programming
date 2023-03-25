c0aa9acea4651c666534c86bf03a8129d89597f3
sedkitech <dhiasedkipro@gmail.com>
2023-03-25 04:15:46 -0700

    optimizing print_strings function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,28 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_strings - prints strings, followed by a new line
- * @separator: string separator to print
- * @n: number of arguments
- * @...: arguments
+ * print_strings - prints strings
+ * @separator: string to be printed between strings
+ * @n: number of args
+ * @...: args
  * Return: nothing
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int i = 0;
-	char *str;
+	unsigned int i;
+	va_list args;
+	char *value;
 
-	va_start(list, n);
-	for (; i < n; i++)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		str = va_arg(list, char*);
-		if (!str)
+		value = va_arg(args, char *);
+		if (value == NULL)
 			printf("(nil)");
-		else
-			printf("%s", str);
-		if (!separator)
-			continue;
-		if (i < n - 1)
+		printf("%s", value);
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
-	printf("\n");
-	va_end(list);
+	putchar('\n');
 }
