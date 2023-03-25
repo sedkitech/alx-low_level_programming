aee945df4497f0f01b1e70003d4a6549a35558dd
sedkitech <dhiasedkipro@gmail.com>
2023-03-25 04:23:54 -0700

    correcting output

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,12 +17,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		value = va_arg(args, char *);
+		value = va_arg(args, char*);
 		if (value == NULL)
 			printf("(nil)");
-		printf("%s", value);
+		else
+			printf("%s", value);
+
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
+	va_end(args);
 	putchar('\n');
 }
