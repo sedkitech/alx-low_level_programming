f52df0a9a6f577e2f6bb4a5c886e010321a3d44e
sedkitech <dhiasedkipro@gmail.com>
2023-03-31 10:39:27 -0700

    adding 100first func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * prints_before_main - prints before the main func
+ * Return: nothing
+ */
+
+void prints_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
