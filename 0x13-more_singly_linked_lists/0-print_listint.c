a69d5608a6a1fa27615439c8433870542b8fe481
sedkitech <dhiasedkipro@gmail.com>
2023-04-04 10:05:12 -0700

    init and adding print_listint function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of struct list
+ * @h: pointer to struct list
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+# General
+___
+* How to use linked lists
+* Start to look for the right source of information without too much help
