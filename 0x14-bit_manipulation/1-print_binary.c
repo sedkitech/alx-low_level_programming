1eac6565b4bc167c9943055302211dd4e4f55c46
sedkitech <dhiasedkipro@gmail.com>
2023-04-11 08:50:57 -0700

    changing printf to _putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,7 +11,7 @@ void print_bits(unsigned long int n)
 	if (n == 0)
 		return;
 	print_bits(n >> 1);
-	printf("%ld", n & 1);
+	_putchar((n & 1) + '0');
 }
 
 /**
@@ -23,7 +23,7 @@ void print_bits(unsigned long int n)
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
-		printf("%ld", n);
+		_putchar('0');
 	else
 		print_bits(n);
 }
