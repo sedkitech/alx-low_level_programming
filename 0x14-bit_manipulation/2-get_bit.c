95fc544d5fa6c9323ca5d527cb4c5f56d0e125e1
sedkitech <dhiasedkipro@gmail.com>
2023-04-12 15:25:55 -0700

    adding 2-git_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: given number
+ * @index: index
+ * Return: index or -1
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int hint = 0;
+
+	if (!index && !n)
+		return (-1);
+
+	if (index == 0)
+		return (n & 1);
+
+	while (n != 0)
+	{
+		if (hint > index)
+		{
+			hint = (-1);
+			break;
+		}
+		if (index == hint)
+		{
+			hint = (n & 1);
+			break;
+		}
+		(n = n >> 1);
+		hint++;
+	}
+	return (hint);
+
+}
